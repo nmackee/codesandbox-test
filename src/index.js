58d87bd5ab23486727a2d301493eae459470d9b1
nmackee <70114875+nmackee@users.noreply.github.com>
2021-12-19 02:52:47 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -202,7 +202,33 @@ const nameArr = ["田中", "山田", "じゃけえ"];
 // const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
 // console.log(formattedNum)
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
-};
-console.log(checkSum(50, 40));
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("１か2はtrueになります。")
+// }
+// if (flag1 && flag2) {
+//   console.log("１も2もtrueになります。")
+// }
+
+// || は左側がfalseなら右側を返す(左側がtrueなら左側を返す)
+// nullはfalse
+// const num = null;
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = null;
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
